ebbca60df2b1fe88f25b641ff0fb32527a6dcc3b
Julian Cheal <julian.cheal@gmail.com>
2014-07-18 04:16:12 +0300

    Basic working version of Leap Music. Controlling audio samples via the gesture command.

--- leap_music.rb
+++ leap_music.rb
@@ -1,26 +1,55 @@
 require 'artoo'
+require 'vlc-client'
 
 connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
 device :leapmotion, :driver => :leapmotion
 
 work do
   on leapmotion, :open => :on_open
-  on leapmotion, :frame => :on_frame
+  on leapmotion, :gesture => :on_gesture
   on leapmotion, :close => :on_close
 end
 
 def on_open(*args)
-  puts args
-end
+pid = fork{ exec 'afplay', "audio/harder_better_faster_stronger.mp3" }
 
-def on_frame(*args)
-  frame = args[1]
-  puts frame
-  puts frame.hands
-  puts frame.gestures
-  puts frame.pointables
-end
+@vlc = VLC::System.new
+@vlc.connect
+@vlc.volume(200)
+@songs = %w{ after.wav
+            better.wav
+            do_it.wav
+            ever.wav
+            faster.wav
+            harder.wav
+            hour.wav
+            make_it.wav
+            makes_us.wav
+            more_than.wav
+            never.wav
+            our.wav
+            over.wav
+            stronger.wav
+            work_is.wav
+            work_it.wav
+            over.wav
+          }
+end 
+
 
 def on_close(*args)
-  puts args
+  @vlc1.disconnect
+  @vlc.disconnect
+end
+
+def on_gesture(*args)
+  gesture = args[1]
+  # puts gesture.type
+  case gesture.type
+    when "keyTap"
+      song = "audio/#{@songs.sample}"
+      puts song
+      @vlc.clear
+      @vlc.play(song) unless @vlc.playing?
+  end
 end
